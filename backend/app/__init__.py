73d4596847380fdce7967744f3cdbcd7fdc41575
Dong-Young Kim <31337.persona@gmail.com>
2021-05-09 02:30:11 +0900

    feat: update application config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
 
@@ -6,9 +8,19 @@ from database import Database
 db = Database()
 
 
-def create_app(environment="development"):
+def create_app(environment: str = "development"):
+    if environment not in ["production", "development", "testing"]:
+        raise RuntimeError("app environment is an invalid.")
+
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_object(f"config.{environment.capitalize()}Config")
+    app.config.from_pyfile("config.py", silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     db.init_app(app)
     api = Api(app)
