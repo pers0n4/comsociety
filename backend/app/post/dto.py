d2947abcf59b4e0769a787eb07901d5f2dceece9
Jiyoon Bak <52561966+jiyoonB@users.noreply.github.com>
2021-05-18 03:37:18 +0900

    fix: Edit CreateUserSchema and UpdateUserSchema

--- backend/app/post/dto.py
+++ backend/app/post/dto.py
@@ -15,16 +15,22 @@ class PostSchema(Schema):
 
 
 class CreatePostSchema(Schema):
+    id = fields.UUID(dump_only=True)
+    author_id = fields.UUID(dump_only=True)
     subject = fields.String(required=True)
     content = fields.String(required=True)
+    created_at = fields.DateTime(dump_only=True)
 
     class Meta:
         ordered = True
 
 
 class UpdatePostSchema(Schema):
+    id = fields.UUID(dump_only=True)
+    author_id = fields.UUID(dump_only=True)
     subject = fields.String()
     content = fields.String()
+    updated_at = fields.DateTime(dump_only=True)
 
     class Meta:
         ordered = True
