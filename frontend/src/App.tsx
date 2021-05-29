5760f98ba7647e48be7062590c9468f2fb31991c
Dong-Young Kim <31337.persona@gmail.com>
2021-05-30 02:01:44 +0900

    feat(board): add board page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { ChakraProvider, Icon, Button, Link } from '@chakra-ui/react';
+import { Button, ChakraProvider, Icon, Link } from '@chakra-ui/react';
 import { IoLogoSnapchat } from 'react-icons/io';
 import {
-  BrowserRouter as Router,
-  Switch,
   Route,
+  BrowserRouter as Router,
   Link as RouterLink,
+  Switch,
 } from 'react-router-dom';
 
 import Chatbot from './components/Chatbot';
@@ -14,6 +14,7 @@ import Roadview from './components/Roadview';
 import Signup from './components/Signup';
 import Container from './components/layouts/Container';
 import Header from './components/layouts/Header';
+import Board from './pages/board';
 import Home from './pages/home';
 
 const App: React.FC = () => {
@@ -21,6 +22,9 @@ const App: React.FC = () => {
     <ChakraProvider>
       {/* <Router>
         <Header>
+          <Link as={RouterLink} to="/board">
+            <Button variant="ghost">Board</Button>
+          </Link>
           <Link as={RouterLink} to="/signup">
             <Button variant="ghost">Signup</Button>
           </Link>
@@ -46,6 +50,8 @@ const App: React.FC = () => {
             <Route path="/changepassword">
               <ChangePassword />
             </Route>
+            <Route component={Home} exact path="/" />
+            <Route component={Board} path="/board" />
             <Route path="/">
               <Home />
             </Route>
