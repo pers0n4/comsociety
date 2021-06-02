507544d87fca570746f2b92eafa0e819113fa792
Dong-Young Kim <31337.persona@gmail.com>
2021-06-03 03:17:04 +0900

    feat: add home alert

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-import { Box, Grid, Heading, Stack, Text } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Grid,
+  Heading,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import { useHistory } from 'react-router-dom';
 
 interface CardProps {
@@ -32,23 +40,29 @@ const LinkCard: React.FC<CardProps> = ({ descriptuon, title, path }) => {
 
 const Home: React.FC = () => {
   return (
-    <Grid gap={4} templateColumns="repeat(3, 1fr)">
-      <LinkCard
-        descriptuon="게시판 기능을 사용할 수 있습니다"
-        path="/board"
-        title="Board"
-      />
-      <LinkCard
-        descriptuon="자연어 처리 기능을 체험해볼 수 있습니다."
-        path="/textrank"
-        title="TextRank"
-      />
-      <LinkCard
-        descriptuon="지도 기능을 사용할 수 있습니다"
-        path="/directions"
-        title="KakaoMap"
-      />
-    </Grid>
+    <>
+      <Alert status="info">
+        <AlertIcon />
+        다크 모드, 챗봇 및 유저 인증 관련 기능 일부가 적용되어 있습니다.
+      </Alert>
+      <Grid gap={4} mt={2} templateColumns="repeat(3, 1fr)">
+        <LinkCard
+          descriptuon="게시판 기능을 사용할 수 있습니다"
+          path="/board"
+          title="Board"
+        />
+        <LinkCard
+          descriptuon="자연어 처리 기능을 체험해볼 수 있습니다."
+          path="/textrank"
+          title="TextRank"
+        />
+        <LinkCard
+          descriptuon="지도 기능을 사용할 수 있습니다"
+          path="/directions"
+          title="KakaoMap"
+        />
+      </Grid>
+    </>
   );
 };
 
